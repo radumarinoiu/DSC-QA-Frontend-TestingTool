3de62b912ba1426bb1860ee3ca7cba049b318278
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2020-04-03 21:24:40 +0300

    Added ACTION_LIST to Scenario

--- TestingTool/scenario.py
+++ TestingTool/scenario.py
@@ -4,6 +4,7 @@ from mongoengine import EmbeddedDocument
 from mongoengine import StringField, EmbeddedDocumentListField
 from .task import Task
 from .result import Result
+from .actions import ACTION_LIST
 
 
 class Scenario(EmbeddedDocument):
