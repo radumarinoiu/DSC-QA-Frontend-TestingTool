1d2ba3a5964d3d2dbecdbe55a0d4c4dab563b119
Iuliana <iulifeif@gmail.com>
2020-04-06 19:02:30 +0300

    Task Scenario Class

--- TestingTool/scenario.py
+++ TestingTool/scenario.py
@@ -4,7 +4,26 @@ from mongoengine import EmbeddedDocument
 from mongoengine import StringField, EmbeddedDocumentListField
 from .task import Task
 from .result import Result
+from selenium import webdriver
 
 
 class Scenario(EmbeddedDocument):
-    pass  # I'm just a placeholder
+    name = StringField(max_length=128, required=True)
+    url = StringField(max_length=512, required=True)
+    tasks = EmbeddedDocumentListField(Task)
+
+    def run(self):
+        driver = webdriver.Firefox()
+        driver.implicity_wait(5)
+        driver.maximize_window()
+        data = []
+        result = True
+        for task in self.tasks:
+            task.run(driver)
+            result = task.result.result
+            if result is False:
+                data.append(task.result.data)
+        if data:
+            result = False
+        response_result = Result(result, data)
+        return response_result
