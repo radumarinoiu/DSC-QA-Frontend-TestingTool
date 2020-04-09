2f31cd8be59f115c7c1a0ba73b42cfc6b1a9df3a
AdamIoana <adam_ioana2000@yahoo.com>
2020-04-09 21:00:19 +0300

    TestSuite Class Task

--- TestingTool/testsuite.py
+++ TestingTool/testsuite.py
@@ -1,5 +1,6 @@
 import json
 
+from mongoengine import EmbeddedDocument
 from mongoengine import Document
 from mongoengine import StringField, EmbeddedDocumentListField
 from .scenario import Scenario
@@ -7,4 +8,20 @@ from .result import Result
 
 
 class TestSuite(Document):
-    pass  # I'm just a placeholder
+    name = StringField(max_length=128, required=True)
+    scenarios = EmbeddedDocumentListField(Scenario)
+
+    def run(self):
+        data = []
+        result = True
+        for scenario in self.scenarios:
+            scenario.run()
+            result = scenario.result.result
+            if result is False:
+                data.append(scenario.result.data)
+
+        if data:
+            result = False
+
+        response_result = Result(result=result, data=data)
+        return response_result
