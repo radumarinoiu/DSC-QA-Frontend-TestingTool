9ddfd71fb4e92ce23fe357beda7c61c57a89ad81
DianaTerchea <44743846+DianaTerchea@users.noreply.github.com>
2020-04-09 11:02:33 +0300

    Task Class Done

--- TestingTool/task.py
+++ TestingTool/task.py
@@ -7,4 +7,20 @@ from .actions import ACTION_LIST
 
 
 class Task(EmbeddedDocument):
-    pass  # I'm just a placeholder
+    name = StringField(max_length=128, required=True)
+    action = StringField(max_length=128, required=True)
+    args = DictField()
+    reverse_result = BooleanField(default=False)
+    result = EmbeddedDocumentField(Result)
+
+
+def run(self, browser_instance):
+    data = DictField()
+    result = BooleanField()
+    for key in ACTION_LIST:
+        if key == self.action:
+            result, data = ACTION_LIST[key](self.args, browser_instance)
+        if self.reverse_result:
+            self.result = Result(not result, data)
+        else:
+            self.result = Result(result, data)
