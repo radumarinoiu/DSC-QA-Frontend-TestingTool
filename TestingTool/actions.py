c3cf3f242c16311c316112fa816332fba2f7256e
David Viziteu <andrei_david7@hotmail.com>
2020-04-09 19:35:10 +0300

    added try except

--- TestingTool/actions.py
+++ TestingTool/actions.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# trebuiau oare si import urile astea?
+from selenium.common.exceptions import TimeoutException
 
 
 # def example_action(action_args, browser_instance):
@@ -14,17 +14,17 @@ def click_element(action_args, browser_instance):
     x_path_val = action_args.get("xpath")
     if x_path_val is None:
         return False, {"err": "You didnt give me an xpath in action_args"}
-    else:
-        wait = WebDriverWait(browser_instance, 2)
+
+    wait = WebDriverWait(browser_instance, 2)
+    try:
         element = wait.until(EC.presence_of_element_located(
             (By.XPATH, x_path_val)
         ))
+    except TimeoutException:
+        return False, {"err": "timeout exception when looking for element at given xpath"}
 
-        if len(element) > 0:
-            element.click()
-            return True, {"success": "clicked on xpath given in action_args"}
-
-        return False, {"err": "element at given xpath didnt appear on the web page"}
+    element.click()
+    return True, {"success": "clicked on xpath given in action_args"}
 
 
 ACTION_LIST = {
