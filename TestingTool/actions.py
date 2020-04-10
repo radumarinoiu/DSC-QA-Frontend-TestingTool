a4af985d43977a3a39332ed2d55e82d58dd81e1d
Iuliana <iulifeif@gmail.com>
2020-04-10 11:55:00 +0300

    I really hope it is done now

--- TestingTool/actions.py
+++ TestingTool/actions.py
@@ -8,10 +8,41 @@ from selenium.common.exceptions import TimeoutException
 #     if some_arg is None:
 #         return False, {"err": "Lipseste argumentul some_arg"}
 
+# def click_element(action_args, browser_instance):
+#     x_path_val = action_args.get("xpath")
+#     if x_path_val is None:
+#         return False, {"err": "You didnt give me an xpath in action_args"}
+#
+#     wait = WebDriverWait(browser_instance, 2)
+#     try:
+#         element = wait.until(EC.presence_of_element_located(
+#             (By.XPATH, x_path_val)
+#         ))
+#     except TimeoutException:
+#         return False, {"err": "Timeout exception when looking for element at given xpath"}
+#
+#     element.click()
+#     return True, {"success": "Clicked on xpath given in action_args"}
+#
+# def input_element(action_args, browser_instance):
+#     x_path_val = action_args.get("xpath")
+#     input_text = action_args.get("input_text")
+#     if x_path_val is None:
+#         return False, {"err": "You didnt give me an xpath in action_args"}
+#     wait = WebDriverWait(browser_instance, 2)
+#     try:
+#         element = wait.until(EC.presence_of_element_located(
+#             (By.XPATH, x_path_val)
+#         ))
+#     except TimeoutException:
+#         return False, {"err": "Timeout exception when looking for element at given xpath"}
+
+#     element = input_text
+#     return True, {"success": "Element from xpath took the received value"}
+
 
 def element_from_xpath(action_args, browser_instance):
     x_path_val = action_args.get("xpath")
-    input_text = action_args.get("input_text")
     if x_path_val is None:
         return False, {"err": "You didnt give me an xpath in action_args"}
     wait = WebDriverWait(browser_instance, 2)
@@ -34,9 +65,12 @@ def click_element(action_args, browser_instance):
 
 def input_element(action_args, browser_instance):
     result, data = element_from_xpath(action_args, browser_instance)
+    input_text = action_args.get("input_text")
     if result is False:
         return result, data
-    data.sendKeys(action_args.get("input_text"))
+    if input_text is None:
+        return False, {"err": {"You didnt give me an input text in action_args"}}
+    data.sendKeys(input_text)
     return True, {"success": "Element from xpath took the received value"}
 
 
