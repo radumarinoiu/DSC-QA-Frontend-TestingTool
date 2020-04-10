e3a2a92c0fd1dcc17de7d5ee3a992a27b3beceaf
Iuliana <iulifeif@gmail.com>
2020-04-10 11:46:16 +0300

    task solved

--- TestingTool/actions.py
+++ TestingTool/actions.py
@@ -8,38 +8,6 @@ from selenium.common.exceptions import TimeoutException
 #     if some_arg is None:
 #         return False, {"err": "Lipseste argumentul some_arg"}
 
-# def click_element(action_args, browser_instance):
-#     x_path_val = action_args.get("xpath")
-#     if x_path_val is None:
-#         return False, {"err": "You didnt give me an xpath in action_args"}
-#
-#     wait = WebDriverWait(browser_instance, 2)
-#     try:
-#         element = wait.until(EC.presence_of_element_located(
-#             (By.XPATH, x_path_val)
-#         ))
-#     except TimeoutException:
-#         return False, {"err": "Timeout exception when looking for element at given xpath"}
-#
-#     element.click()
-#     return True, {"success": "Clicked on xpath given in action_args"}
-#
-# def input_element(action_args, browser_instance):
-#     x_path_val = action_args.get("xpath")
-#     input_text = action_args.get("input_text")
-#     if x_path_val is None:
-#         return False, {"err": "You didnt give me an xpath in action_args"}
-#     wait = WebDriverWait(browser_instance, 2)
-#     try:
-#         element = wait.until(EC.presence_of_element_located(
-#             (By.XPATH, x_path_val)
-#         ))
-#     except TimeoutException:
-#         return False, {"err": "Timeout exception when looking for element at given xpath"}
-
-#     element = input_text
-#     return True, {"success": "Element from xpath took the received value"}
-
 
 def element_from_xpath(action_args, browser_instance):
     x_path_val = action_args.get("xpath")
@@ -53,23 +21,28 @@ def element_from_xpath(action_args, browser_instance):
         ))
     except TimeoutException:
         return False, {"err": "Timeout exception when looking for element at given xpath"}
-    return element
+    return True, element
 
 
 def click_element(action_args, browser_instance):
-    element = element_from_xpath(action_args, browser_instance)
-    element.click()
+    result, data = element_from_xpath(action_args, browser_instance)
+    if result is False:
+        return result, data
+    data.click()
     return True, {"success": "Clicked on xpath given in action_args"}
 
 
 def input_element(action_args, browser_instance):
-    element = element_from_xpath(action_args, browser_instance)
-    element = action_args.get("input_text")
+    result, data = element_from_xpath(action_args, browser_instance)
+    if result is False:
+        return result, data
+    data.sendKeys(action_args.get("input_text"))
     return True, {"success": "Element from xpath took the received value"}
 
 
 ACTION_LIST = {
     # "example_action": example_action,
+    # element_from_xpath does not meet the standard to be here
     "click_element": click_element,
     "input_element": input_element
 }
