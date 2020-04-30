ba67d2cf5123966e90f02a34403f689a1c7a3f5c
AdamIoana <adam_ioana2000@yahoo.com>
2020-04-30 15:26:27 +0300

    From Folder - TestSuite Class Task
    Added "from_folder" method

--- TestingTool/testsuite.py
+++ TestingTool/testsuite.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from mongoengine import EmbeddedDocument
 from mongoengine import Document
@@ -25,3 +26,14 @@ class TestSuite(Document):
 
         response_result = Result(result=result, data=data)
         return response_result
+
+    def from_folder(self, directory):
+        file_names = os.listdir(directory)
+
+        for file_name in file_names:
+            file_path = os.path.join(directory, file_name)
+            with open(file_path, "r") as file:
+                scenario = Scenario.from_json(file.read())
+                self.scenarios.append(scenario)
+
+
