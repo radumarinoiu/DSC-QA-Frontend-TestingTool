fc9d56273a4fa19dfa7ddf5edae215efa37d8a44
Iuliana <iulifeif@gmail.com>
2020-05-14 11:33:58 +0300

    fixed from folder

--- TestingTool/testsuite.py
+++ TestingTool/testsuite.py
@@ -12,9 +12,8 @@ class TestSuite(Document):
     scenarios_folder = StringField(required=False)
 
     def run(self):
-        scenarios_folder = self.scenarios_folder
-        if scenarios_folder:
-            self.from_folder(directory=scenarios_folder)
+        if self.scenarios_folder:
+            self.scenarios = self.from_folder(directory=self.scenarios_folder)
         for scenario in self.scenarios:
             result = scenario.run()
             if result.result:
@@ -22,11 +21,14 @@ class TestSuite(Document):
             else:
                 print("Scenario {} failed, reason:\n{}".format(scenario.name, json.dumps(result.data, indent=2)))
 
-    def from_folder(self, directory):
+    @staticmethod
+    def from_folder(directory):
         file_names = os.listdir(directory)
-
+        scenarios = []
+        
         for file_name in file_names:
             file_path = os.path.join(directory, file_name)
             with open(file_path, "r") as file:
                 scenario = Scenario.from_json(file.read())
-                self.scenarios.append(scenario)
+                scenarios.append(scenario)
+        return scenarios
